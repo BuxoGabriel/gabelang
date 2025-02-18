0693c35171e491d272d52fefa2f2779d1aa81265
BuxoGabriel <gabrielbuxo1@gmail.com>
2025-02-18 16:00:18 -0500

    added built-in function reverse for strings and arrays

--- src/evaluator/built_ins.rs
+++ src/evaluator/built_ins.rs
@@ -5,7 +5,8 @@ use crate::evaluator::{GabrEnv, ObjectType};
 
 pub fn load_built_ins() -> HashMap<String, Rc<dyn BuiltIn>> {
     let mut hash_map = HashMap::new();
-    hash_map.insert("len".to_string(), Rc::from(Len{}.as_built_in()));
+    hash_map.insert("len".to_string(), Len{}.as_built_in());
+    hash_map.insert("reverse".to_string(), Reverse{}.as_built_in());
     hash_map
 }
 
@@ -27,7 +28,7 @@ impl BuiltIn for Len {
     fn eval(&self, env: &mut GabrEnv) -> Result<ObjectType, String> {
         let obj = env.get_var("_obj");
         if obj.is_none() {
-            return Err("Built-In \"len\" expected did not recieve expected arg \"_arr\"".to_string())
+            return Err("Built-In \"len\" did not recieve expected arg \"_arr\"".to_string())
         };
         match obj.unwrap() {
             ObjectType::ARRAY(arr) => Ok(ObjectType::NUMBER(arr.len() as i64)),
@@ -36,6 +37,30 @@ impl BuiltIn for Len {
         }
     }
 
+    fn as_built_in(self) -> Rc<dyn BuiltIn> {
+        Rc::from(self)
+    }
+}
+
+struct Reverse{}
+
+impl BuiltIn for Reverse {
+    fn get_params(&self) -> Vec<String> {
+        vec![
+            "_obj".to_string(),
+        ]
+    }
+    fn eval(&self, env: &mut GabrEnv) -> Result<ObjectType, String> {
+        let object = env.get_var("_obj");
+        if object.is_none() {
+            return Err("Built-In \"reverse\" did not recieve expected arg \"_obj\"".to_string())
+        }
+        match object.unwrap() {
+            ObjectType::STRING(string) => Ok(ObjectType::STRING(string.chars().rev().collect())),
+            ObjectType::ARRAY(arr) => Ok(ObjectType::ARRAY(arr.clone().into_iter().rev().collect())),
+            _ => Err("Built-In \"reverse\" expects first arg\"str\" to be of type string".to_string())
+        }
+    }
     fn as_built_in(self) -> Rc<dyn BuiltIn> {
         Rc::from(self)
     }
